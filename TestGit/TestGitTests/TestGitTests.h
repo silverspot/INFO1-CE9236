74784fb06b3096a5427355c7c5c056866c25ab4d
Benson Rajan <bnsn626@gmail.com>
2011-10-13 17:49:53 -0400

    Commit

---
+++ TestGit/TestGitTests/TestGitTests.h
@@ -0,0 +1,13 @@
+//
+//  TestGitTests.h
+//  TestGitTests
+//
+//  Created by Benson Rajan on 10/13/11.
+//  Copyright 2011 UNFCU. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TestGitTests : SenTestCase
+
+@end
---
+++ TestGit/TestGitTests/TestGitTests.m
@@ -0,0 +1,32 @@
+//
+//  TestGitTests.m
+//  TestGitTests
+//
+//  Created by Benson Rajan on 10/13/11.
+//  Copyright 2011 UNFCU. All rights reserved.
+//
+
+#import "TestGitTests.h"
+
+@implementation TestGitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TestGitTests");
+}
+
+@end
